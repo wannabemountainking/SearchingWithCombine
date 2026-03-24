9d598e105ef549828e9a1d7543f796eb4154aafd
wannabemountainking <kdy0096@gmail.com>
2026-03-24 16:53:46 +0900

    Initial Commit

---
+++ SearchingWithCombine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchingWithCombine
+//
+//  Created by YoonieMac on 3/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SearchingWithCombine/SearchingWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchingWithCombineApp.swift
+//  SearchingWithCombine
+//
+//  Created by YoonieMac on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct SearchingWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
